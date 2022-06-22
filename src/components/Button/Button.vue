30cbb8ce641b75f52936cde6558a35792d8be175
Eduardo Nowakoski <eduardo_nowa@hotmail.com>
2022-06-22 20:00:56 -0300

    style: reset css

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+        <button class="button">{{msgButton}}</button>
+  </div>
+</template>
+
+<script>
+export default {
+    // eslint-disable-next-line
+    name: "Button",
+    props: {
+        msgButton: {
+            typeof:String,
+            default: "Continuar"
+        }
+    }
+}
+</script>
+
+<style lang="scss" scooped>
+@import './Button.scss'
+
+</style>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -3,7 +3,9 @@
     <div class="form">
     <Inputs typeInput="email" placeholder="Usuário"/>
     <Inputs typeInput= "password" placeholder="Senha"/>
+    
     <Span />
+    <Button />
   </div>
   </div>
 </template>
@@ -11,13 +13,15 @@
 <script>
 import Inputs from '@/components/Inputs/Inputs.vue'
 import Span from '@/components/Span/Span.vue'
+import Button from '@/components/Button/Button.vue'
 
 export default {
   // eslint-disable-next-line
   name: 'Home',
   components: {
     Inputs,
-    Span
+    Span,
+    Button
   }
 }
 </script>
