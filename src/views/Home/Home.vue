a80f31ea7ad0c1190a44c5973f126e2d26a2efc2
Leonardo-Luis-Klein <leonardo.klein.pb@compasso.com.br>
2022-06-23 16:15:20 -0300

    Feat:Title logic

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -6,6 +6,10 @@
     
     <Span />
     <Button />
+    <Title nameClass="smallTitle" tMensage= "Our mission is"/>
+    <Title nameClass="bigTitlle" tMensage= "to transform the world"/>
+    <Title nameClass="bigTitlle" tMensage= "builidng digital experiences"/>
+    <Title nameClass="bigTitlle" tMensage= "that enable our client's growth"/>
   </div>
   </div>
 </template>
@@ -14,6 +18,7 @@
 import Inputs from '@/components/Inputs/Inputs.vue'
 import Span from '@/components/Span/Span.vue'
 import Button from '@/components/Button/Button.vue'
+import Title from '@/components/Title/Title.vue'
 
 export default {
   // eslint-disable-next-line
@@ -21,8 +26,9 @@ export default {
   components: {
     Inputs,
     Span,
-    Button
-  }
+    Button,
+    Title
+}
 }
 </script>
 
