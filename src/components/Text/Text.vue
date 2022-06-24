1155ab43cd60ccd81bda33b7ca833cf58702ae3f
Maicon Bresolin <106255646+MaiconLBresolin@users.noreply.github.com>
2022-06-24 14:11:21 -0300

    feat:text component

---
+++ src/components/Text/Text.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <p type="text" :class="textClass"> {{ text }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Text',
+        props:{
+            type:{
+                type: String,
+            },
+            textClass:{
+                type: String,
+            },
+            text:{
+                type: String,
+                default: 'null',
+            },
+
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+@import './Text.scss';
+</style>
--- src/views/Success/Success.vue
+++ src/views/Success/Success.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    
+    <Text /> 
   </div>
 </template>
 
@@ -10,7 +10,7 @@ export default {
     name: 'Succcess'
 }
 </script>
-
+  import Text from "@/components/Text/Text.vue";
 <style>
 
 </style>
