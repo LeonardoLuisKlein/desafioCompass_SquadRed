961df948f220c104e690bc06f02625c582db8c51
Leonardo-Luis-Klein <leonardo.klein.pb@compasso.com.br>
2022-06-24 15:16:27 -0300

    Feat: Instaling axios and start develop weather component

---
+++ src/components/Header/Weather/Weather.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Passo Fundo - RS</h1>
+    <img />
+    <h1>22°</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Weather',
+}
+</script>
+
+<style>
+
+</style>
