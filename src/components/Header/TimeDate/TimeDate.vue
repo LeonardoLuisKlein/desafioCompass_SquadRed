f2b8e8bd956af1e770dda991b69186ca16aa333d
Eduardo Nowakoski <eduardo_nowa@hotmail.com>
2022-06-25 20:03:16 -0300

    feat: logic for realtime clock

--- src/components/Header/TimeDate/TimeDate.vue
+++ src/components/Header/TimeDate/TimeDate.vue
@@ -12,16 +12,33 @@ export default {
     return {
       clock: "",
       date: "",
+      hours: "",
+      minutes: "",
+      day: "",
+      month:'',
+
     };
   },
   methods: {
+    getHour() {
+      let actualDate = new Date();
+      let hours = actualDate.getHours();
+      let minutes = actualDate.getMinutes();
+      let clock = `${hours} : ${minutes}`;
+      this.clock = clock;
+    },
+
+    getDate(){
+      const months = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+      const days = ['segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado', 'domingo',]
 
 
-
-
-
-
-
+    }
+  },
+  created() {
+    setInterval(() => {
+      this.getHour();
+    }, 1000);
   },
 };
 </script>
