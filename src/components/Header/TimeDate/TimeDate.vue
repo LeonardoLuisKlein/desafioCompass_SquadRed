5caaba737c98eb091f03a22ea41eb7dcf1a0e86c
Eduardo Nowakoski <eduardo_nowa@hotmail.com>
2022-06-25 21:11:18 -0300

    fix: fixing time when the value was less then 2 digits

--- src/components/Header/TimeDate/TimeDate.vue
+++ src/components/Header/TimeDate/TimeDate.vue
@@ -23,7 +23,7 @@ export default {
       let date = new Date();
       let hours = date.getHours();
       let minutes = date.getMinutes();
-      let clock = `${hours}:${minutes}`;
+      let clock = `${hours <= 9 ? '0' :''}${hours}:${minutes <= 9 ? '0' : ''}${minutes}`;
       this.clock = clock;
 
       const months = [
