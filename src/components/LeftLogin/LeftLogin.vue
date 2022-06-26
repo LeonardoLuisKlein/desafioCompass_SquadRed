6ba519b987d63190f9e75e5a7e306f031384ee23
Eduardo Nowakoski <eduardo_nowa@hotmail.com>
2022-06-25 23:31:13 -0300

    doc: creating right login and left login components

---
+++ src/components/LeftLogin/LeftLogin.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="left-login">
+    <div id="logo1">
+      <img
+        draggable="false"
+        src="@/assets/logo.png"
+        class="disable-select"
+        alt=""
+      />
+    </div>
+    <div class="welcome disable-select">
+      <h1 class="title">Olá,</h1>
+      <p class="paragraph">
+        Para continuar navegando de forma segura, efetue o login na rede.
+      </p>
+    </div>
+    <div class="form">
+      <h2 class="disable-select">Login</h2>
+      <Inputs type="text" placeholder="Usuário" idInput="userInput" />
+      <Inputs type="password" placeholder="Senha" idInput="pwInput" />
+      <Span />
+      <Button />
+    </div>
+  </div>
+</template>
+
+<script>
+import Inputs from "@/components/Inputs/Inputs.vue";
+import Span from "@/components/Span/Span.vue";
+import Button from "@/components/Button/Button.vue";
+
+export default {
+  name: "LeftLogin",
+  components: {
+    Inputs,
+    Span,
+    Button
+  }
+};
+</script>
+
+<style lang="scss" scooped>
+@import "./LeftLogin.scss";
+</style>
--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,42 +1,19 @@
 <template>
   <div class="login">
-    <div class="left-login">
-      <div id="logo1">
-        <img draggable="false" src="@/assets/logo.png" class="disable-select" alt="" />
-      </div>
-      <div class="welcome disable-select"  >
-        <h1 class="title">Olá,</h1>
-        <p class="paragraph">
-          Para continuar navegando de forma segura, efetue o login na rede.
-        </p>
-      </div>
-      <div class="form">
-        <h2 class="disable-select">Login</h2>
-        <Inputs type="text" placeholder="Usuário" idInput="userInput" />
-        <Inputs type="password" placeholder="Senha" idInput="pwInput" />
-        <Span />
-        <Button />
-      </div>
-    </div>
-    <div class="right-login">
-      <div id="logo" class="disable-select">
-        <img draggable="false" src="@/assets/logo.png" alt="" />
-      </div>
-    </div>
+    <LeftLogin />
+    <RightLogin />
   </div>
 </template>
 <script>
-import Inputs from "@/components/Inputs/Inputs.vue";
-import Span from "@/components/Span/Span.vue";
-import Button from "@/components/Button/Button.vue";
+import LeftLogin from "@/components/LeftLogin/LeftLogin.vue";
+import RightLogin from "@/components/RightLogin/RightLogin.vue";
 
 export default {
   // eslint-disable-next-line
   name: "Login",
   components: {
-    Inputs,
-    Span,
-    Button,
+    LeftLogin,
+    RightLogin,
   },
   methods: {},
 };
