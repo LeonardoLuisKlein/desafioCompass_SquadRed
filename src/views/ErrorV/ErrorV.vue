73c98409bbfeb696115a67496455a090006b006e
Eduardo Nowakoski <eduardo_nowa@hotmail.com>
2022-06-27 19:11:42 -0300

    feat: adding error page

---
+++ src/views/ErrorV/ErrorV.vue
@@ -0,0 +1,17 @@
+<template>
+  <div  class="erro disable-select">
+    <h1>Error 401</h1>
+    <h2>Unauthorized Access</h2>
+    <h3>You must be logged in to access this page.</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ErrorV",
+};
+</script>
+
+<style lang="scss" scooped>
+@import "./ErrorV.scss";
+</style>
