74413d16f4d3c1c2ce1de149f45270d86a1cc599
Maicon Bresolin <106255646+MaiconLBresolin@users.noreply.github.com>
2022-06-29 12:44:50 -0300

    adding feat:main

---
+++ src/components/Main/Main.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="mainContainer">
+    <div class="main">
+      <div class="mainLeft">
+        <img src="@/assets/compass.png" alt="Logo Compass">
+      </div>
+      <div class="mainRight">
+        <Title class="sucessTitleSmall" type="h1" tMensage="Our mission is" />
+        <Texts class="sucessText" msg="Nossa missão é" />
+        <Title class="sucessTitleBig" type="h2" tMensage="to transform the world" />
+        <Texts class="sucessText" msg="Transformar o mundo" />
+        <Title class="sucessTitleBig" type="h3" tMensage="building digital experiences" />
+        <Texts class="sucessText" msg="construindo experiências digitais" />
+        <Title class="sucessTitleBig" type="h4" tMensage="that enable our client's growth" />
+        <Texts class="sucessText" msg="que permitam o crescimento dos nossos clientes" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Texts from "@/components/Texts/Texts.vue";
+import Title from "@/components/Title/Title.vue";
+
+export default {
+  // eslint-disable-next-line
+  name: "Main",
+  components:{
+    Texts,
+    Title,
+  }
+}
+</script>
+
+<style lang="scss" scooped>
+@import "./Main.scss";
+</style>
