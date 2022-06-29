80889ce8305b91a663c8e53562dcfd31a72e3de1
Eduardo Nowakoski <eduardo_nowa@hotmail.com>
2022-06-29 16:59:12 -0300

    fix: fixing responsivty of main component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,35 @@
 <template>
   <div id="app">
-    <router-view />
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
+    <router-view/>
   </div>
 </template>
 
+
 <script>
 export default {
-  name: "App",
-};
+  name: 'App'
+}
 </script>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+
+html, body, #app {
+  height: 100%;
+}
 
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: "Poppins", sans-serif;
+  font-family: 'Poppins', sans-serif;
 }
 
 .disable-select {
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
+  -webkit-user-select: none;  
+  -moz-user-select: none;    
+  -ms-user-select: none;      
   user-select: none;
 }
 
