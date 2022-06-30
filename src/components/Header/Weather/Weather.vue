99b5d6c2409db8b8242278f31d8d4b1b3a3c31ba
Eduardo Nowakoski <eduardo_nowa@hotmail.com>
2022-06-29 23:07:35 -0300

    chore: removing clgs in weather component

--- src/components/Header/Weather/Weather.vue
+++ src/components/Header/Weather/Weather.vue
@@ -1,98 +1,93 @@
 <template>
   <div class="container">
     <div class="cityTitle disable-select">
-    <p >{{tMensage=`${local} - ${state}`}}</p>
+      <p>{{ (tMensage = `${local} - ${state}`) }}</p>
     </div>
     <div class="tempContainer">
       <div class="condition">
-        <img v-if="imageName" draggable="false" :src="require(`@/assets/${imageName}.png`)" alt="Weather status icon" class="weatherIcon disable-select"/>
+        <img
+          v-if="imageName"
+          draggable="false"
+          :src="require(`@/assets/${imageName}.png`)"
+          alt="Weather status icon"
+          class="weatherIcon disable-select"
+        />
       </div>
-    <div class="temperatureContent disable-select" draggable="false">
-    <p draggable="false">{{tMensage=`${temp}°`}}</p > 
-        </div>
+      <div class="temperatureContent disable-select" draggable="false">
+        <p draggable="false">{{ (tMensage = `${temp}°`) }}</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    // eslint-disable-next-line
-    name: "Weather",
-    data(){
-      return{
-        imageName: "sun",
-        local: "Três Arroios",
-        region: "",
-        lat: "",
-        long: "",
-        dadosLocal: "",
-        addIcon: "",
-        temp: "28",
-        state: "RS",
-        dadosCity: "",
-      }
-    },
-    methods: {
-    getWeather(){
-        if('geolocation' in navigator){
-            navigator.geolocation.getCurrentPosition((position) => {
-              let lat = position.coords.latitude;
-              let long = position.coords.longitude;
-              console.log(lat, long)
+  // eslint-disable-next-line
+  name: "Weather",
+  data() {
+    return {
+      imageName: "sun",
+      local: "Três Arroios",
+      region: "",
+      lat: "",
+      long: "",
+      dadosLocal: "",
+      addIcon: "",
+      temp: "28",
+      state: "RS",
+      dadosCity: "",
+    };
+  },
+  methods: {
+    getWeather() {
+      fetch("http://ip-api.com/json/?fields=city,region,lat,lon")
+        .then((resposta) => resposta.json())
+        .then((dadosCity) => {
+          this.state = dadosCity.region;
+          this.local = dadosCity.city;
+          this.lat = dadosCity.lat;
+          this.long = dadosCity.lon;
 
-              fetch(
-                "http://ip-api.com/json/?fields=city,region,lat,lon"
-              )
-              .then((resposta) => resposta.json())
-              .then((dadosCity) => {
-                this.state = dadosCity.region
-                this.local = dadosCity.city
-                this.lat = dadosCity.lat
-                this.long = dadosCity.lon
-                console.log(this.lat, this.long)
-
-            fetch(
-              `http://api.weatherapi.com/v1/current.json?key=b3972d7c329b490c9c1175956222706&q=${this.lat},${this.long}`
-            )
+          fetch(
+            `http://api.weatherapi.com/v1/current.json?key=b3972d7c329b490c9c1175956222706&q=${this.lat},${this.long}`
+          )
             .then((resposta) => resposta.json())
             .then((dados) => {
-              this.temp = dados.current.temp_c.toFixed(0)
-              console.log(this.temp)
-              this.imageName = this.getIconWeather(dados.current.condition.text);
-              console.log(dados)
-              })
-            })
-          })
-        }     
+              this.temp = dados.current.temp_c.toFixed(0);
+
+              this.imageName = this.getIconWeather(
+                dados.current.condition.text
+              );
+            });
+        });
     },
 
-    getIconWeather(weather){
-      let icon
+    getIconWeather(weather) {
+      let icon;
 
-      if (weather.includes("thunder")){
+      if (weather.includes("thunder")) {
         icon = "storm";
-      } 
-      else if (weather.includes("Sun") || weather.includes("Clear")){
+      } else if (weather.includes("Sun") || weather.includes("Clear")) {
         icon = "sun";
-      } 
-      else if (weather.includes("Fog") || weather.includes("Mist")){
+      } else if (weather.includes("Fog") || weather.includes("Mist")) {
         icon = "cloud";
-      } 
-      else if (weather.includes("Cloudy") || weather.includes("Overcast") || weather.includes("cloudy")){
+      } else if (
+        weather.includes("Cloudy") ||
+        weather.includes("Overcast") ||
+        weather.includes("cloudy")
+      ) {
         icon = "cloudy";
-      } 
-      else if (weather.includes("rain") || weather.includes("drizzle")){
+      } else if (weather.includes("rain") || weather.includes("drizzle")) {
         icon = "raining";
       }
-      return icon
-    }
+      return icon;
+    },
+  },
 
-},
-
-created() {
+  created() {
     this.getWeather();
-  }
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
